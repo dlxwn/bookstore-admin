1afcd00968ccf1c22e1dca9d3682e829b6ebe76c
qmxwby <2621955028@qq.com>
2020-06-08 22:02:07 +0800

    网上书店后台管理前端初始化

---
+++ src/components/Home.vue
@@ -0,0 +1,212 @@
+<template>
+  <el-container>
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="togleCollapse">|||</div>
+        <el-menu unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath" background-color="#333744" text-color="#fff" active-text-color="#409FFF">
+           <!-- :unique-opened="true"->只允许展开一个菜单 -->
+           <!-- :collapse-transition="false" -> 关闭动画 -->
+           <!-- router -> 导航开启路由模式 -->
+          <!-- 一级菜单  -->
+          <el-submenu index="1" >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <i class="iconfont icon-user"></i>
+              <span>用户管理</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="/users" @click="saveNavState('/users')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2" >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <i class="iconfont icon-tijikongjian"></i>
+              <span>权限管理</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="/rights" @click="saveNavState('/rights')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>权限列表</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="/roles" @click="saveNavState('/roles')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>角色列表</span>
+              </template>
+            </el-menu-item>
+        </el-submenu>
+          <el-submenu index="3" >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <i class="iconfont icon-shangpin"></i>
+              <span>图书管理</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="/goods" @click="saveNavState('/goods')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>商品列表</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="/params" @click="saveNavState('/params')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>分类参数</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="/categories" @click="saveNavState('/categories')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>商品分类</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4" >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <i class="iconfont icon-danju"></i>
+              <span>订单管理</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="/orders" @click="saveNavState('/orders')">
+              <!-- 导航开启路由模式：
+                将index值作为导航路由 -->
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>订单列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 内容主体 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      iconObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      // 默认不折叠
+      isCollapse: false,
+      // 被激活导航地址
+      activePath: ''
+    }
+  },
+  created () {
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  methods: {
+    logout () {
+      // 清空token
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    // 菜单的折叠与展开
+    togleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存连接的激活地址
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373f41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      height: 40px;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+
+  .el-menu {
+    border: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont{
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  // 鼠标放上去变成小手
+  cursor: pointer;
+}
+</style>
